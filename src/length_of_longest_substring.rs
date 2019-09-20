23a443cb47654630bc63e8e36984eb63d03107a9
crupest <crupest@outlook.com>
2019-09-20 20:28:55 +0800

    Optimize p3.

--- src/length_of_longest_substring.rs
+++ src/length_of_longest_substring.rs
@@ -1,29 +1,27 @@
 use super::Solution;
 
-use std::collections::HashMap;
-
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        let mut map = HashMap::new();
-        let mut last_index = 0;
-        let mut result = 0;
+        let mut map: [i32; std::u8::MAX as usize] = [-1; std::u8::MAX as usize];
+        let mut last_index: i32 = 0;
+        let mut result: i32 = 0;
         let bytes = s.as_bytes();
         for (i, c) in bytes.iter().enumerate() {
-            match map.get(&c) {
-                Some(vi) if *vi >= last_index => {
-                    last_index = *vi + 1;
-                    map.insert(c, i);
-                }
-                _ => {
-                    map.insert(c, i);
-                    let length = i - last_index + 1;
-                    if length > result {
-                        result = length;
-                    }
+            let i = i as i32;
+            let c = *c as usize;
+            let li = map[c];
+            if li >= last_index {
+                last_index = li + 1;
+                map[c] = i;
+            } else {
+                map[c] = i;
+                let length = i - last_index + 1;
+                if length > result {
+                    result = length;
                 }
             }
         }
-        result as i32
+        result
     }
 }
 
